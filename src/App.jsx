d1768d479d58946c5a14923079497a0d515254a2
Willy Hardian <hardian.will@gmail.com>
2024-10-07 11:04:35 +0700

    feat: pokemon pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,55 @@
 import "./App.css";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { QueryClient, useQuery, useQueryClient } from "@tanstack/react-query";
 import Card from "./components/Card";
+import { useEffect, useState } from "react";
 
 function App() {
+    const queryClient = new QueryClient();
+    const limit = 20;
+    const offset = 20;
+    const [page, setPage] = useState(1);
+    const [hasMore, setHasMore] = useState(true);
     const { isPending, error, data } = useQuery({
-        queryKey: ["pokemons"],
+        queryKey: ["pokemons", page - 1],
         queryFn: async () => {
-            const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+            const response = await fetch(
+                `https://pokeapi.co/api/v2/pokemon?offset=${
+                    offset * (page - 1)
+                }&limit=${limit}`
+            );
             if (!response.ok) {
                 throw new Error("Network response was not ok");
             }
             const responseJson = await response.json();
             return responseJson;
         },
+        staleTime: 5000,
     });
+    useEffect(() => {
+        async function fetchData() {
+            await queryClient.prefetchQuery({
+                queryKey: ["pokemons", page],
+                queryFn: async () => {
+                    const response = await fetch(
+                        `https://pokeapi.co/api/v2/pokemon?offset=${
+                            offset * page
+                        }&limit=${limit}`
+                    );
+                    if (!response.ok) {
+                        throw new Error("Network response was not ok");
+                    }
+                    const responseJson = await response.json();
+                    if (responseJson.results.length === 0) {
+                        setHasMore(false);
+                    } else {
+                        setHasMore(true);
+                    }
+                    return responseJson;
+                },
+            });
+        }
+        fetchData();
+    }, [data]);
 
     return (
         <>
@@ -30,6 +66,22 @@ function App() {
                         })}
                     </div>
                 )}
+                <div className="flex justify-center pagination">
+                    <button
+                        className="btn"
+                        disabled={page - 1 === 0}
+                        onClick={() => setPage((page) => page > 0 && page - 1)}
+                    >
+                        Previous
+                    </button>
+                    <button
+                        className="btn"
+                        disabled={!hasMore}
+                        onClick={() => setPage((page) => hasMore && page + 1)}
+                    >
+                        Next
+                    </button>
+                </div>
             </div>
         </>
     );
